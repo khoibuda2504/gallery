c198bfe95971c3ec88da8097e89f2e0e92ab41a3
khoibuda2504 <68694998+khoibuda2504@users.noreply.github.com>
2022-11-29 14:56:53 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ for (i = 1; i <= 14; i++) {
   container.append(div)
 }
 
-for (i = 1; i <= 14; i++) {
+for (i = 1; i <= 26; i++) {
   const div = document.createElement('div')
   div.classList.add("card", `card-${sizeArr[Math.floor(Math.random() * 3)]}`)
   div.innerHTML = `<img data-src="./src/${i}.jpg" />`
@@ -31,14 +31,17 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
   })
 }, {
   threshold: 0,
-  rootMargin: "0px 0px -300px 0px"
+  rootMargin: "0px 0px -100px 0px"
 })
 
 images.forEach(image => {
   imgObserver.observe(image)
 })
+
+
 document.querySelectorAll('img').forEach(image => {
   image.addEventListener('click', () => {
+    if(image.getAttribute('data-click') === 'none') return
     document.getElementById('popup-image').style.display = 'block'
     document.querySelector('.popup-image img').src = image.getAttribute('src')
   })
@@ -50,3 +53,17 @@ window.addEventListener('keydown', (e) => {
   if (e.keyCode !== 27) return
   document.getElementById('popup-image').style.display = 'none'
 })
+
+
+const scrollTopEle = document.getElementById('scroll-top')
+scrollTopEle.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  })
+})
+
+window.addEventListener("scroll", () => {
+  scrollTopEle.style.display = window?.scrollY > 30 ? 'flex' : 'none'
+}, false)
